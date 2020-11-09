0353bcc33bad2231627f8af8dc5443563cd49bde
Chris Martin <bmg.martin08@gmail.com>
2020-11-09 23:30:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,8 @@ end
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
 def long_planeteer_calls(planeteer_calls)
-  answer = 
+  answer = false 
+  
 end
 
 
