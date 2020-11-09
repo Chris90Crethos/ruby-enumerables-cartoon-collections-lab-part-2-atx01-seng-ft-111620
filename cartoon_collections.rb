572b4160c024150d7c46905db7aa51b1fd1cc64b
Chris Martin <bmg.martin08@gmail.com>
2020-11-09 23:32:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,5 +38,8 @@ end
   # Return the first valid call found, or return nil if no valid calls are found
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+end
 end
